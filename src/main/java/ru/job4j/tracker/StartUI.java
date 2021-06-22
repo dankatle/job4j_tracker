7827eb4ff2f2d9140afdd1542c2afa016d59d914
Daniil <dan.kattsyn@gmail.com>
2021-06-22 13:11:38 +0300

    5. Tracker - хранилище [#396]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -1,11 +1,12 @@
 package ru.job4j.tracker;
 
 import java.time.format.DateTimeFormatter;
+import java.util.Arrays;
 
 public class StartUI {
     public static void main(String[] args) {
-        Item item = new Item();
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MMMM-EEEE-yyyy HH:mm:ss");
-        System.out.println(item.getCreated().format(formatter));
+        Tracker tracker = new Tracker();
+        tracker.add(new Item("Задача"));
+        System.out.println(Arrays.toString(tracker.findByName("Задача")));
     }
 }
--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -1,5 +1,7 @@
 package ru.job4j.tracker;
 
+import java.util.Arrays;
+
 public class Tracker {
     private final Item[] items = new Item[100];
     private int ids = 1;
@@ -9,6 +11,30 @@ public class Tracker {
         item.setId(ids++);
         items[size++] = item;
         return item;
+    }
+
+    public Item[] findAll() {
+        Item[] itemsWithoutNull = new Item[size];
+        int count = 0;
+        for (int i = 0; i < size; i++) {
+            if (items[i] != null) {
+                itemsWithoutNull[i] = items[i];
+                count++;
+            }
+        }
+        return Arrays.copyOf(itemsWithoutNull, count);
+    }
+
+    public Item[] findByName(String key) {
+        Item[] itemsSortByName = new Item[size];
+        int count = 0;
+        for (int i = 0; i < size; i++) {
+            if (key.equals(items[i].getName())) {
+                itemsSortByName[i] = items[i];
+                count++;
+            }
+        }
+        return Arrays.copyOf(itemsSortByName, count);
     }
 
     public Item findById(int id) {
