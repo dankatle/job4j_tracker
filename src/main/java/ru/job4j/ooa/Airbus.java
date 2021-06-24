d889a26bb3d3afae42a49fe2f2c8b2c00d66fac0
Daniil <dan.kattsyn@gmail.com>
2021-06-24 11:35:02 +0300

    4. final [#334073]

---
+++ src/main/java/ru/job4j/ooa/Airbus.java
@@ -0,0 +1,37 @@
+package ru.job4j.ooa;
+
+public final class Airbus {
+    private static final int COUNT_ENGINE = 2;
+
+    private String name;
+
+    public Airbus(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void printModel() {
+        System.out.println("Модель самолета: " + name);
+    }
+
+    public void printCountEngine() {
+        if ("A380".equals(name)) {
+            System.out.println("Количество двигателей равно: " + 4);
+        }
+        System.out.println("Количество двигателей равно: " + COUNT_ENGINE);
+    }
+
+    @Override
+    public String toString() {
+        return "Airbus{"
+                + "name='" + name + '\''
+                + '}';
+    }
+}
