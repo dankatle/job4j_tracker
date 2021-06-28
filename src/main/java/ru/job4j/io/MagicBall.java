570d33c5cec140ef704128df78e7490dc287e4b7
Daniil <dan.kattsyn@gmail.com>
2021-06-28 09:46:55 +0300

    fix travis build error

--- src/main/java/ru/job4j/io/MagicBall.java
+++ src/main/java/ru/job4j/io/MagicBall.java
@@ -6,10 +6,12 @@ public class MagicBall {
     public static void main(String[] args) {
         System.out.print("Я великий Оракул. Что ты хочешь узнать? ");
         int answer = new Random().nextInt(3);
-        switch (answer) {
-            case 0 -> System.out.println("Да");
-            case 1 -> System.out.println("Нет");
-            default -> System.out.println("Может быть");
+        if (answer == 0) {
+            System.out.println("Да");
+        } else if (answer == 1) {
+            System.out.println("Нет");
+        } else {
+            System.out.println("Может быть");
         }
     }
 }
