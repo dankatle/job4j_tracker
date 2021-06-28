1c5dcd2f18bff5373faf1988602b24b94abedc79
Daniil <dan.kattsyn@gmail.com>
2021-06-28 09:39:44 +0300

    fix travis build error

--- src/main/java/ru/job4j/io/MagicBall.java
+++ src/main/java/ru/job4j/io/MagicBall.java
@@ -1,13 +1,10 @@
 package ru.job4j.io;
 
 import java.util.Random;
-import java.util.Scanner;
 
 public class MagicBall {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
         System.out.print("Я великий Оракул. Что ты хочешь узнать? ");
-        String in = scanner.nextLine();
         int answer = new Random().nextInt(3);
         switch (answer) {
             case 0 -> System.out.println("Да");
