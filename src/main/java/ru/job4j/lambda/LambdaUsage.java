a85c3ffa6efce588c58c0d45251305e09ed0eca8
Daniil <dan.kattsyn@gmail.com>
2021-06-30 09:59:09 +0300

    2.2. Лямбда блок [#249208 #172914]
    fix

--- src/main/java/ru/job4j/lambda/LambdaUsage.java
+++ src/main/java/ru/job4j/lambda/LambdaUsage.java
@@ -5,7 +5,7 @@ import java.util.Comparator;
 public class LambdaUsage {
     public static void main(String[] args) {
         Comparator<String> cmpText = (left, right) -> {
-            System.out.println("compare " + left + " and " + right);
+            System.out.println("compare " + right + " and " + left);
             return right.compareTo(left);
         };
     }
