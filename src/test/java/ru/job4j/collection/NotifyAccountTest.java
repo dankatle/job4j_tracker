3a23189214ff66f06e06238d02b7c9b6f2f5386f
Daniil <dan.kattsyn@gmail.com>
2021-07-02 14:24:20 +0300

    1. Метод .of [#70623]

--- src/test/java/ru/job4j/collection/NotifyAccountTest.java
+++ src/test/java/ru/job4j/collection/NotifyAccountTest.java
@@ -5,6 +5,7 @@ import org.junit.Test;
 import java.util.Arrays;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.*;
@@ -12,29 +13,24 @@ import static org.junit.Assert.*;
 public class NotifyAccountTest {
     @Test
     public void sent() {
-        List<Account> accounts = Arrays.asList(
+        List<Account> accounts = List.of(
                 new Account("123", "Petr Arsentev", "eDer3432f"),
                 new Account("142", "Petr Arsentev", "000001")
         );
-        HashSet<Account> expect = new HashSet<>(
-                Arrays.asList(
-                        new Account("123", "Petr Arsentev", "eDer3432f"),
-                        new Account("142", "Petr Arsentev", "000001")
-                )
-        );
+        Set<Account> expect = Set.of(
+                new Account("123", "Petr Arsentev", "eDer3432f"),
+                new Account("142", "Petr Arsentev", "000001"));
         assertThat(NotifyAccount.sent(accounts), is(expect));
     }
 
     @Test
     public void whenAccountsDuplicate() {
-        List<Account> accounts = Arrays.asList(
+        List<Account> accounts = List.of(
                 new Account("123", "Petr Arsentev", "eDer3432f"),
                 new Account("123", "Petr Arsentev", "eDer3432f")
         );
-        HashSet<Account> expect = new HashSet<>(
-                Arrays.asList(
-                        new Account("123", "Petr Arsentev", "eDer3432f")
-                )
+        Set<Account> expect = Set.of(
+                new Account("123", "Petr Arsentev", "eDer3432f")
         );
         assertThat(NotifyAccount.sent(accounts), is(expect));
     }
--- src/test/java/ru/job4j/collection/OrderConvertTest.java
+++ src/test/java/ru/job4j/collection/OrderConvertTest.java
@@ -5,6 +5,7 @@ import org.junit.Test;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.*;
@@ -12,18 +13,18 @@ import static org.junit.Assert.*;
 public class OrderConvertTest {
     @Test
     public void whenSingleOrder() {
-        List<Order> orders = new ArrayList<>();
-        orders.add(new Order("3sfe", "Dress"));
-        HashMap<String, Order> map = OrderConvert.process(orders);
+        List<Order> orders = List.of(new Order("3sfe", "Dress"));
+        Map<String, Order> map = OrderConvert.process(orders);
         assertThat(map.get("3sfe"), is(new Order("3sfe", "Dress")));
     }
 
     @Test
     public void whenDuplicateOrder() {
-        List<Order> orders = new ArrayList<>();
-        orders.add(new Order("3sfe", "Dress"));
-        orders.add(new Order("3sfe", "Dress"));
-        HashMap<String, Order> map = OrderConvert.process(orders);
+        List<Order> orders = List.of(
+                new Order("3sfe", "Dress"),
+                new Order("3sfe", "Dress")
+        );
+        Map<String, Order> map = OrderConvert.process(orders);
         assertEquals(map.size(), 1);
     }
 }
